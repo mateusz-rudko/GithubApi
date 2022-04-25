2f430ad3d35239c7e17c4384dcbc4d8692c009ca
Mateusz Rudko <mateuszrudko@gmail.com>
2022-04-25 22:36:34 +0200

    allegro intership

---
+++ AllegroApi/Models/LanguageSize.cs
@@ -0,0 +1,9 @@
+﻿namespace AllegroApi.Models
+{
+    public class LanguageSize
+    {
+        public string Language { get; set; }
+        public int Size { get; set; }
+        
+    }
+}
---
+++ AllegroApi/Models/Repo.cs
@@ -0,0 +1,10 @@
+﻿namespace AllegroApi.Models
+{
+    public class Repo
+    {
+        public string Full_Name { get; set; }
+        public string Language { get; set; }
+        public int Size { get; set; }
+        
+    }
+}
